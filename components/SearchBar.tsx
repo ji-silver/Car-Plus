c3330b7a4bef0192700c5bc9d4544e8458dfb9dc
jisilver <jisilver.kim@gmail.com>
2023-08-25 22:08:02 +0900

    Feat: 차량 검색 시 url 쿼리 설정

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,19 +1,77 @@
 "use client"
 import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 import SearchManufacturer from "./SearchManufacturer"
+import { RiCarFill } from 'react-icons/ri'
+import { BiSearch } from 'react-icons/bi'
+
+// 컴포넌트 따로 생성 안 하고 바로 함수로 JSX 생성
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+    <button type="submit" className={`ml-3 z-10 ${otherClasses} w-5 h-5`}>
+        <BiSearch className="w-full h-full" />
+    </button>
+)
 
 const SearchBar = () => {
     const [manufacturer, setManufacturer] = useState('')
-    const handleSearch = () => {
+    const [model, setModel] = useState('')
+    const router = useRouter();
 
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        // 검색창이 빈칸이면 alert return 
+        if (manufacturer === '' && model === '') {
+            return alert("빈칸을 입력해주세요.")
+        }
+        // 검색 버튼을 누르면 updateSearchParams() 함수 실행, manufacturer(제조사), model 소문자로 변경해서 넘겨주기
+        updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase())
     }
+
+    const updateSearchParams = (model: string, manufacturer: string) => {
+        // 현재 URL에 대한 객체 생성하기 (? 물음표 다음에 오는 쿼리 부분 설정)
+        const searchParams = new URLSearchParams(window.location.search);
+
+        // 모델이 있으면 쿼리 매개변수로 현재 model 생성. 없으면 삭제
+        if (model) {
+            searchParams.set('model', model)
+        } else {
+            searchParams.delete('model')
+        }
+
+        // 제조사가 있으면 쿼리 매개변수로 manufacturer 생성, 없으면 삭제 
+        if (manufacturer) {
+            searchParams.set('manufacturer', manufacturer)
+        } else {
+            searchParams.delete('manufacturer')
+        }
+
+        // 새로운 URL 경로 생성 (현재 경로 + searchParams)
+        const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+        // 새로운 경로로 페이지 이동
+        router.push(newPathname)
+    }
+
     return (
         <form className='searchbar' onSubmit={handleSearch}>
             <div className="searchbar__item">
                 <SearchManufacturer manufacturer={manufacturer} setManufacturer={setManufacturer} />
+                <SearchButton otherClasses="sm:hidden" />
+            </div>
+            <div className="searchbar__item">
+                <div className='flex items-center flex-1 max-sm:w-full'>
+                    <div className='absolute w-5 h-5 text-[#515151] ml-4'>
+                        <RiCarFill className="w-full h-full" />
+                    </div>
+                    <input type="text" name='model' value={model} onChange={(e) => setModel(e.target.value)} placeholder='Tiguan' className='searchbar__input' />
+                    <SearchButton otherClasses="sm:hidden" />
+                </div>
+                <SearchButton otherClasses="max-sm:hidden" />
             </div>
         </form>
     )
 }
 
 export default SearchBar
+
--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -3,24 +3,22 @@ import { useState, Fragment } from 'react'
 import { SearchManufacturerProps } from '@/types'
 import { Combobox, Transition } from '@headlessui/react'
 import { manufacturers } from '@/constants'
-import { BiSearch } from 'react-icons/bi'
+import { CgBmw } from 'react-icons/cg'
 import Image from 'next/image'
+
 
 // Type 가져오기
 const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
     const [query, setQuery] = useState('')
-    console.log(manufacturer)
 
     // 제조사 목록 필터링
     // 검색란이 비어있는 경우 manufacturers 목록 그대로 가져오기
     // 그렇지 않은 경우 입력 query와 일치하는 걸 필터링
     // 필터링은 query에서 대소문자, 공백 제거한 결과를 includes()를 이용해서 포함되는지 여부 확인하고 일치하면 filteredManufacturers 배열에 포함시키기
-    const filteredManufacturers = query === ""
-        ? manufacturers
-        : manufacturers.filter((item) => (
-            item.korean.includes(query) || item.english.toLowerCase().replace(/\s+/g, "").includes(query.toLowerCase().replace(/\s+/g, ""))
-        ));
-
+    const filteredManufacturers =
+        query === "" ? manufacturers : manufacturers.filter((item) =>
+            item.toLowerCase().replace(/\s+/g, "").includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
 
     // bomobox에서 제조사를 선택하면 manufacturer 값으로 들어감
     return (
@@ -29,19 +27,31 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                 <div className='relative w-full'>
                     <Combobox.Button className="absolute top-[14px]">
                         <div className='ml-4 w-5 h-5 text-[#515151]'>
-                            <BiSearch className="w-full h-full" />
+                            <CgBmw className="w-full h-full" />
                         </div>
                     </Combobox.Button>
-                    <Combobox.Input className="search-manufacturer__input" placeholder='기아' displayValue={(manufacturer: { korean: string }) => manufacturer.korean} onChange={(e) => setQuery(e.target.value)} />
+                    <Combobox.Input
+                        className='search-manufacturer__input'
+                        displayValue={(item: string) => item}
+                        onChange={(event) => setQuery(event.target.value)}
+                        placeholder='Kia'
+                    />
                     <Transition as={Fragment} leave='transition ease-in duration-100' leaveFrom='opacity-100' leaveTo='opacity-10' afterLeave={() => setQuery('')}>
                         <Combobox.Options>
                             {/* active 되면 스타일링 주기 */}
                             {
                                 filteredManufacturers.map((item) => (
-                                    <Combobox.Option key={item.korean} className={({ active }) => `relative search-manufacturer__option ${active ? `bg-primary-blue text-white` : 'text-gray-900'}`} value={item}>{({ selected, active }) => (
+                                    <Combobox.Option
+                                        key={item}
+                                        className={({ active }) =>
+                                            `relative search-manufacturer__option ${active ? "bg-primary-blue text-white" : "text-gray-900"
+                                            }`
+                                        }
+                                        value={item}
+                                    >{({ selected, active }) => (
                                         <>
                                             <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
-                                                {item.korean}
+                                                {item}
                                             </span>
                                             {selected ? (
                                                 <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? "text-white" : "text-primary-blue"}`}
@@ -56,7 +66,6 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
 
                 </div>
             </Combobox>
-
         </div>
     )
 }
